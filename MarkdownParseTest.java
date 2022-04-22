37b86a376124456ef8c737832497e9bb0cb6b9cd
CobyLsy <cobylinlsy@gmail.com>
2022-04-21 17:31:37 -0700

    All tests passed

---
+++ MarkdownParseTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.junit.*;
+
+public class MarkdownParseTest extends MarkdownParse{
+    private String content;
+    private ArrayList<String> Links;
+    Path filename;
+
+
+    @Before
+    public void setUp() throws IOException{
+        filename = Path.of("test-file.md");
+        content = Files.readString(filename);
+        Links = getLinks(content);
+    }
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinksTest() {
+        List<String> list = List.of("https://something.com", "some-thing.html");
+        assertEquals(list, Links);
+    }
+
+}
